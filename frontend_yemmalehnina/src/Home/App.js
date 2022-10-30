6937f69069e5aab461dd2317ba1ea4f98d9c8e34
oussama-soulimani <o.soulimani01@gmail.com>
2022-10-30 22:42:51 +0100

    mod

--- frontend_yemmalehnina/src/Home/App.js
+++ frontend_yemmalehnina/src/Home/App.js
@@ -114,7 +114,7 @@ function App() {
   const [response, setResponse] = useState(null)
   const [name, setName] = useState("")
   const handleClick = ()=>{
-    fetch("http://localhost:8080/api/product", {method:"GET"})
+    fetch("http://localhost:8080/api/products", {method:"GET"})
     .then((res)=>(res.json())).then(response=>setName(response[0].name))
   }
   
