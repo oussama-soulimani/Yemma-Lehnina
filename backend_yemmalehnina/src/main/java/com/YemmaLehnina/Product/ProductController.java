417023443ba51b6b81c576b0dd6a58f8a00aee0d
oussama-soulimani <o.soulimani01@gmail.com>
2022-10-31 16:50:40 +0100

    mods

--- backend_yemmalehnina/src/main/java/com/YemmaLehnina/Product/ProductController.java
+++ backend_yemmalehnina/src/main/java/com/YemmaLehnina/Product/ProductController.java
@@ -2,7 +2,6 @@ package com.YemmaLehnina.Product;
 
 // API Layer
 
-import org.aspectj.lang.annotation.DeclareError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
